89a7fb3de73df252588eafab7d80a65d20278f7e
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2026-04-27 23:55:35 -0300

    Ajustando componente SnippetsHeader storybook

--- bytebank/src/components/Snippets/SnippetsHeader.stories.ts
+++ bytebank/src/components/Snippets/SnippetsHeader.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
 import SnippetsHeader from './header';
 
 const meta: Meta<typeof SnippetsHeader> = {
@@ -6,6 +7,14 @@ const meta: Meta<typeof SnippetsHeader> = {
   component: SnippetsHeader,
   parameters: {
     layout: 'fullscreen',
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        push: fn(),
+        replace: fn(),
+        prefetch: fn(),
+      },
+    },
   },
   tags: ['autodocs'],
 };
@@ -13,4 +22,14 @@ const meta: Meta<typeof SnippetsHeader> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Desktop: Story = {
+  parameters: {
+    viewport: { defaultViewport: 'desktop' },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: { defaultViewport: 'mobile1' },
+  },
+};
